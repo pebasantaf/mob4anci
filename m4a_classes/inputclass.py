2307f436b698741936a4ef3bc340e18fefd7cf36
pebasantaf <pedrobasantafranco@outlook.com>
2022-05-19 11:18:55 +0200

    modification of inputclass to process power/energy and efficiency

--- m4a_classes/inputclass.py
+++ m4a_classes/inputclass.py
@@ -1,5 +1,7 @@
+import statistics
+
 class inputInfo:
-    def __init__(self=None, id=None, storagetype=None, chargetype=None, chargeefficiency=None, dischargeefficiency=None, 
+    def __init__(self, id=None, storagetype=None, chargetype=None, chargeefficiency=None, dischargeefficiency=None, 
     energy=None, power=None, remotecontrol=None, rampup=None, connectivity=None):
         self.id = id
         self.storagetype = storagetype
@@ -11,5 +13,44 @@ class inputInfo:
         self.remotecontrol = remotecontrol
         self.rampup = rampup
         self.connectivity = connectivity
+        
+    def getRealPower(self, flow):
+        
+        if flow == 'discharge':
+            
+            realpower = round(self.power * self.dischargeefficiency, 4)
+            
+        elif flow == 'charge':
+            
+            realpower = round(self.power * self.chargeefficiency, 4)
+            
+        elif flow == 'symmetric':
 
+            realpower = round(self.power * statistics.mean([self.chargeefficiency, self.dischargeefficiency]), 4)
+            
+        else:
+            
+            raise NameError("Incorrect input string for checkControlReserve")
+            
+        return realpower
+    
+    def getRealEnergy(self, flow):
+        
+        if flow == 'discharge':
+            
+            realenergy = round(self.energy * self.dischargeefficiency, 4)
+            
+        elif flow == 'charge':
+            
+            realenergy = round(self.energy * self.chargeefficiency, 4)
+            
+        elif flow == 'symmetric':
+
+            realenergy = round(self.energy * statistics.mean([self.chargeefficiency, self.dischargeefficiency]),4) 
+            
+        else:
+            
+            raise NameError("Incorrect input string for checkControlReserve")
+            
+        return realenergy
     
